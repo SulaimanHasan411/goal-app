8fcbc6e6ddf34dc7bad0522eb655bf2f9b9ee83c
Sulaiman <SulaimanHasan411@gmail.com>
2025-05-24 16:01:21 -0400

    setup basic express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,7 @@
-console.log("hello worls");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = 5001;
+
+const app = express();
+
+app.listen(port, () => console.log(`server started on port ${port}`));
