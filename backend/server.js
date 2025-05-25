03c21028f3391584da5b034b08b6f417c5bf00d4
Sulaiman <SulaimanHasan411@gmail.com>
2025-05-24 20:38:47 -0400

    get req, setup port

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const port = 5001;
+const port = process.env.PORT || 8001;
 
 const app = express();
+
+app.get('/api/goals', (req, res) => {
+    res.status(200).json({ message: 'get goals' });
+});
 
 app.listen(port, () => console.log(`server started on port ${port}`));
