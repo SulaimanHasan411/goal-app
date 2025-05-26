eed4d6a26c48fa05b9c5dabe5c50174f75147cd6
Sulaiman <SulaimanHasan411@gmail.com>
2025-05-26 00:00:21 -0400

    generate jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -43,6 +43,7 @@ const registerUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         });
     }
     else{
@@ -69,6 +70,7 @@ const loginUser = asyncHandler(async (req, res) => {
             _id: user.id,
             name: user.name,
             email: user.email,
+            token: generateToken(user._id),
         });
     }
     else{
@@ -83,6 +85,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
     res.json({ message: 'User data display'});
 });
+
+// generate jwt
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    });
+};
 
 module.exports = {
     registerUser,
