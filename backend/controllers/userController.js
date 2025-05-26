c590cec3cb06399d954e92b5270081ce72b92859
Sulaiman <SulaimanHasan411@gmail.com>
2025-05-25 20:50:37 -0400

    authenticate user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,7 +55,26 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route   POST /api/users/login
 // @access  public 
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: 'Login user'});
+    if(!req.body){
+        res.status(400);
+        throw new Error('Invalid credentials');
+    }
+    const {email, password} = req.body;
+
+    // check for user email
+    const user = await User.findOne({email});
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+        });
+    }
+    else{
+        res.status(400);
+        throw new Error('Invalid credentials');
+    }
 });
 
 // @desc    get user data
