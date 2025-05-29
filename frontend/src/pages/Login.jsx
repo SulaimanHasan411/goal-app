7be4a5f7f32826fa3afa8dd901571eb0fd9ceeae
Sulaiman <SulaimanHasan411@gmail.com>
2025-05-29 12:07:26 -0400

    fix register functionality, add logout functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,10 @@
 import {useState, useEffect} from 'react';
 import {FaSignInAlt} from 'react-icons/fa';
+import {useSelector, useDispatch} from 'react-redux';
+import {useNavigate, userNavigate} from 'react-router-dom';
+import {toast, toastify} from 'react-toastify';
+import {login, reset} from '../features/auth/authSlice';
+import Spinner from '../components/Spinner';
 
 function Login() {
     const[formData, setFormData] = useState({
@@ -7,7 +12,27 @@ function Login() {
         password: '',
     });
 
-    const { email, password } = formData;
+    const {email, password} = formData;
+
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector(
+        (state) => state.auth
+    );
+
+    useEffect(() => {
+        if(isError){
+            toast.error(message);
+        }
+        
+        if(isSuccess || user){
+            navigate('/');
+        }
+
+        dispatch(reset());
+
+    }, [user, isError, isSuccess, message, navigate, dispatch]);
 
     const onChange = (e) => {
         setFormData((prevState) => ({
@@ -18,9 +43,20 @@ function Login() {
 
     const onSubmit = (e) => {
         e.preventDefault();
+
+        const userData = {
+            email,
+            password
+        }
+
+        dispatch(login(userData));
     };
 
-    return <>
+    if(isLoading){
+        return <Spinner />;
+    }
+
+    return (<>
         <section className='heading'>
             <h1>
                 <FaSignInAlt /> Login
@@ -49,6 +85,6 @@ function Login() {
             </form>
         </section>
     </>
-}
+)}
 
 export default Login;
