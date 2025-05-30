77bf3046753f4f0f1f10273c73fec9ca4e663cbe
Sulaiman <SulaimanHasan411@gmail.com>
2025-05-30 00:46:30 -0400

    add functionality to fetch goals from server

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -17,10 +17,21 @@ export const createGoal = createAsyncThunk('goals/create',
             return await goalService.createGoal(goalData, token);
         } catch (error) {
             const message = (error.response && error.response.data && 
-            error.response.data.message || error.message || error.toString());
-        return thunkAPI.rejectWithValue(message);
+                error.response.data.message || error.message || error.toString());
+            return thunkAPI.rejectWithValue(message);
         }
 }); 
+
+export const getGoals = createAsyncThunk('goals/getAll', async (_, thunkAPI) => {
+    try {
+        const token = thunkAPI.getState().auth.user.token;
+        return await goalService.getGoals(token);
+    } catch (error) {
+        const message = (error.response && error.response.data && 
+            error.response.data.message || error.message || error.toString());
+        return thunkAPI.rejectWithValue(message);
+    }
+});
 
 export const goalSlice = createSlice({
     name: 'goal',
@@ -39,6 +50,19 @@ export const goalSlice = createSlice({
                 state.goals.push(action.payload);
             })
             .addCase(createGoal.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+            })
+            .addCase(getGoals.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getGoals.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.goals = action.payload;
+            })
+            .addCase(getGoals.rejected, (state, action) => {
                 state.isLoading = false;
                 state.isError = true;
                 state.message = action.payload;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,27 +1,47 @@
 import {useEffect} from "react";
 import {useNavigate} from "react-router-dom";
-import {useSelector} from "react-redux";
+import {useSelector, useDispatch} from "react-redux";
 import GoalForm from '../components/GoalForm';
+import Spinner from '../components/Spinner';
+import {getGoals} from '../features/goals/goalSlice';
+import {reset} from '../features/auth/authSlice';
 
 function Dashboard() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const {user} = useSelector((state) => state.auth);
+    const {goals, isError, isLoading, message} = useSelector((state) => state.goals);
 
     useEffect(() => {
+        if(isError){
+            console.log(message);
+        }
+
         if(!user){
             navigate('/login');
         }
-    }, [user, navigate]);
 
-    return <>
+        dispatch(getGoals());
+
+        return () => {
+            dispatch(reset())
+        };
+
+    }, [user, navigate, isError, message, dispatch]);
+
+    if(isLoading){
+        return <Spinner />;
+    }
+
+    return (<>
         <section className="heading">
             <h1>Welcome {user && user.name}</h1>
             <p>Goals Dashboard</p>
         </section>
 
         <GoalForm />
-    </>
+    </>)
 }
 
 export default Dashboard;
