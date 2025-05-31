8da545089ef8e82e65d8d6efca478d4f51338184
Sulaiman <SulaimanHasan411@gmail.com>
2025-05-31 12:43:51 -0400

    go to older version of express for stable path-to-regexp usage

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const colors = require('colors');
 const {errorHandler} = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
-const port = process.env.PORT || 8001;
+const port = process.env.PORT || 5001;
 
 connectDB();
 
@@ -12,8 +13,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded( { extended: false } ));
 
-app.use('/api/goals', require('./routes/goalRoutes'));
-app.use('/api/users', require('./routes/userRoutes'));
+//app.use('/api/goals', require('./routes/goalRoutes'));
+//app.use('/api/users', require('./routes/userRoutes'));
+
+// serve frontend
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 
+        'frontend', 'build', 'index.html')));
+}
+else {
+    app.get('/', (req, res) => res.send('Not set in production'));
+}
 
 app.use(errorHandler);
 
